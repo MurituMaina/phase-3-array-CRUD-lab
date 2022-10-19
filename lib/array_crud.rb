4c98c014f585f42ed4334ff19efbf43898a1b577
MurituMaina <vincent.maina@student.moringaschool.com>
2022-10-19 15:27:54 +0300

    complete requirements adjust crud lab

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,12 +27,13 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-    # array[0]
-    array.first
+    # array.first
+    array[0]
   
 end
 
 def retrieve_last_element_from_array(array)
-#   array[-1]
-array.last
+# array.last
+  array[-1]
+
 end
